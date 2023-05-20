cfc903930c4c028dc08423a21e8dad9dcf16a963
stevebiczyk <stevebiczyk@gmail.com>
2023-05-20 22:12:26 +0000

    Handling Events Challenge copmleted

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Sidebar() {
     return (
         <div>
-            <a href = "#." target= "_blank">My Photos</a>
-            <a href = "#." target= "_blank">My Illustrations</a>
-            <a href = "#." target= "_blank">My Paintings</a>
+            <a href="#." target="_blank">My Photos</a>
+            <a href="#." target="_blank">My Illustrations</a>
+            <a href="#." target="_blank">My Paintings</a>
         </div>
     )
 }
